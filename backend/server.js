26a20877c6a43b440df077087adcab9ff0a22236
Andriy <andriynovak20072007@gmail.com>
2026-05-20 22:35:38 +0300

    deploy

--- backend/server.js
+++ backend/server.js
@@ -113,11 +113,8 @@ app.get('*', (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-if (process.env.NODE_ENV !== 'test') {
-    app.listen(PORT, () => {
-        logger.success('SERVER', `Server працює на порту ${PORT}`);
-    });
-}
-
+app.listen(PORT, () => {
+    logger.success('SERVER', `Server працює на порту ${PORT}`);
+});
 // ОБОВ'ЯЗКОВО ділимося додатком з іншими файлами (для Supertest)
 module.exports = app;
