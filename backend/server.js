67594ddd5e7486ba1f7e84f4adc9180402df9157
Andriy <andriynovak20072007@gmail.com>
2026-05-21 00:16:54 +0300

    deploy4

--- backend/server.js
+++ backend/server.js
@@ -107,9 +107,8 @@ app.use((err, req, res, next) => {
 
 // Запускаємо сервер ТІЛЬКИ якщо це не тести
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, '../frontend/index.html'));
+    res.sendFile(path.join(__dirname, '../frontend/css/index.html'));
 });
-
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
